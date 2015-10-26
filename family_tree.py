1ebaf1d3c5bb544c87d1b7f8a17b3c463add3bb4
Nathan Panchal <nathanpanchal@gmail.com>
2015-10-25 22:20:41 -0700

    add loop functionality

--- family_tree.py
+++ family_tree.py
@@ -1,34 +1,47 @@
-root = {"name": "Bob", "hair": "Brown", "children": ["John", "Guys"]}
+root = {"name": "brown", "hair": "brown", "children": [{'name': 'john', 'hair': 'brown'}, {'name': 'lisa', 'hair': 'red'}]}
 def print_children(person):
     if person["children"]:
+        # will refactor the line below to print each child out
         print("Children: ", person["children"])
     else:
+        # Prints the text below only to show that the node has no children
         print("Children: ")
+
+def check_dupicate_child(array, child_name):
+    for child in array:
+        if child["name"] == child_name:
+            return True
 
 def display_person(person):
     print("Name: ", person["name"])
     print("Hair: ", person["hair"])
+    print_children(person)
 
-def ask_for_child():
+def ask_for_child(root):
+    current_person = root
     child_name = input("Please enter the child's name (or 'return' or 'exit'... ")
-    return child_name
+    if child_name == "exit":
+        print("exiting")
+        quit()
+        print("this should not display")
+    elif child_name == "return":
+        print("returning")
+        display_person(current_person)
+        ask_for_child()
+    elif check_dupicate_child(current_person["children"], child_name):
+        print("this child name exists")
+        current_person = current_person["children"]["name"]
+        ask_for_child(current_person)
+    else:
+        name = child_name
+        hair = input("Hair color?...")
+        new_child = {"name": name, "hair": hair}
+        current_person["children"].append(new_child)
+        display_person(current_person)
+        ask_for_child(current_person)
 
 current_person = root
-child_name = ask_for_child()
-if child_name == "exit":
-    print("exiting")
-    quit()
-    print("this should not display")
-#     exit the program
-elif child_name == "return":
-    print("returning")
-    display_person(current_person)
-#     print the previous person and ask for child name
-else:
-    name = child_name
-    hair = input("Hair color?...")
-    new_child = {"name": name, "hair": hair}
-    current_person["children"].append(new_child)
-    display_person(current_person)
+display_person(current_person)
+ask_for_child(current_person)
     
 
