c83f5605a8fee1906daec2efea2cb2f6cdf6fccd
Nathan Panchal <nathanpanchal@gmail.com>
2015-10-26 21:46:28 -0700

    refactor based on prompt change

--- family_tree.py
+++ family_tree.py
@@ -1,47 +1,30 @@
-root = {"name": "brown", "hair": "brown", "children": [{'name': 'john', 'hair': 'brown'}, {'name': 'lisa', 'hair': 'red'}]}
+root = {"name": "bob", "hair": "brown", "children": [{"name": "Jane"}, {"name": "David"}]}
+
+# makes sure that if the list of children is blank the correct string is displayed
 def print_children(person):
     if person["children"]:
-        # will refactor the line below to print each child out
-        print("Children: ", person["children"])
+        for child in person["children"]:
+            print(" " + child["name"])
     else:
-        # Prints the text below only to show that the node has no children
         print("Children: ")
 
-def check_dupicate_child(array, child_name):
+def check_child_exists(array, child_name):
     for child in array:
         if child["name"] == child_name:
             return True
 
 def display_person(person):
     print("Name: ", person["name"])
-    print("Hair: ", person["hair"])
+    print("Hair color: ", person["hair"])
     print_children(person)
 
-def ask_for_child(root):
-    current_person = root
-    child_name = input("Please enter the child's name (or 'return' or 'exit'... ")
-    if child_name == "exit":
-        print("exiting")
-        quit()
-        print("this should not display")
-    elif child_name == "return":
-        print("returning")
-        display_person(current_person)
-        ask_for_child()
-    elif check_dupicate_child(current_person["children"], child_name):
-        print("this child name exists")
-        current_person = current_person["children"]["name"]
-        ask_for_child(current_person)
-    else:
-        name = child_name
-        hair = input("Hair color?...")
-        new_child = {"name": name, "hair": hair}
-        current_person["children"].append(new_child)
-        display_person(current_person)
-        ask_for_child(current_person)
+def prompt():
+    name = input('Enter a child\'s name, or \"return\", or \"exit\":')
+    return name
 
 current_person = root
 display_person(current_person)
-ask_for_child(current_person)
-    
-
+prompt()
+while True
+    display_person(current_person)
+    choice = prompt
